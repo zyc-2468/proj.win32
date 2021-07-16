a9ec6ccf825ba203b3f2ad466410176633cd977c
zyc-2468 <1942864461@qq.com>
2021-07-16 11:11:58 +0800

    7.16 11:11

--- square.cpp
+++ square.cpp
@@ -1,10 +1,10 @@
 #include"square.h"
 
 Mygraph::Mygraph() {
-	for (int i = 0; i < 16; i++)squares[i][0].type = 1;
-	for (int i = 0; i < 16; i++)squares[i][23].type = 1;
-	for (int j = 0; j < 24; j++)squares[0][j].type = 1;
-	for (int j = 0; j < 24; j++)squares[15][j].type = 1;
+	for (int i = 0; i < 16; i++)squares[i][0].type = 2;
+	for (int i = 0; i < 16; i++)squares[i][23].type = 2;
+	for (int j = 0; j < 24; j++)squares[0][j].type = 2;
+	for (int j = 0; j < 24; j++)squares[15][j].type = 2;
 	for (int i = 3; i < 10; i++)squares[i][5].type = 1;
 	for (int i = 7; i < 13; i++)squares[i][7].type = 1;
 	for (int i = 6; i < 12; i++)squares[i][10].type = 1;
@@ -16,5 +16,13 @@ Mygraph::Mygraph() {
 	for (int j = 4; j < 11; j++)squares[7][j].type = 1;
 	for (int j = 5; j < 9; j++)squares[10][j].type = 1;
 	for (int j = 9; j < 16; j++)squares[13][j].type = 1;
-	
+	squares[7][3].door1 = Mydoor(1, 0, 3, 1);
+	squares[7][17].button = Mybutton(1, 3, 7, 3);
+	squares[11][20].candle = Mycandle(1, 0, 0);
+	squares[13][21].type = 4;
+	squares[13][19].type = 3;
+	squares[12][19].type = 3;
+	squares[11][19].type = 3;
+	squares[10][19].type = 3;
+
 }
--- square.h
+++ square.h
@@ -3,10 +3,49 @@
 
 #include"library.h"
 
+class point{
+public:
+	int i;
+	int j;
+	point(int i, int j) :i(i), j(j) {}
+};
+
+class Mybutton {
+public:
+	int hasbutton;
+	int color;
+	point doorpos;
+	Mybutton() :hasbutton(0), color(0), doorpos(0,0) {}
+	Mybutton(int hasbutton, int color, int i, int j) :hasbutton(hasbutton), color(color), doorpos(i,j) {}
+};
+
+class Mycandle {
+public:
+	int hascandle;
+	point doorpos;
+	Mycandle() :hascandle(0), doorpos(0, 0) {}
+	Mycandle(int hascandle, int i, int j) :hascandle(hascandle), doorpos(i,j) {}
+};
+
+class Mydoor {
+public:
+	int hasdoor;
+	int color;  //0-4: red, yellow, pink, blue,green
+	int needbutton = 1;
+	int nowbutton = 0;
+	int status = 0;
+	Mydoor() :hasdoor(0), color(0), needbutton(1),nowbutton(0),status(0) {}
+	Mydoor(int hasdoor, int color, int index, int needbutton) :hasdoor(hasdoor), color(color), needbutton(needbutton),nowbutton(0),status(0) {}
+};
 
 class Mysquare {
 public:
 	int type = 0;
+	Mycandle candle;
+	Mydoor door1;
+	Mydoor door2;
+	Mybutton button;
+	int hasbox;
 };
 
 
@@ -15,8 +54,6 @@ public:
 	Mysquare squares[16][24];
 	Mygraph();
 };
-
-
 
 #endif
 
